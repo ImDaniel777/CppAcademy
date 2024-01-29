296c8c6ae8cc26e3b0049d7ade9615f1a06c2fe0
ImDaniel777 <iancudaniel99@yahoo.com>
2024-01-29 13:23:20 +0200

    shared memory without boost

--- multiprocessing-shared-memory/main.cpp
+++ multiprocessing-shared-memory/main.cpp
@@ -1,61 +1,51 @@
-#include <boost/interprocess/shared_memory_object.hpp>
-#include <boost/interprocess/mapped_region.hpp>
-#include <boost/interprocess/managed_shared_memory.hpp>
 #include <iostream>
-#include <cstring>
-#include <cstdlib>
-#include <string>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <thread>
 
-int main(int argc, char *argv[])
+void read_shared_memory()
 {
-   using namespace boost::interprocess;
+    std::cout<<"Thread joined"<<'\n';
+    // ftok to generate unique key
+    key_t key = ftok("shmfile", 65);
+ 
+    // shmget returns an identifier in shmid
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+ 
+    // shmat to attach to shared memory
+    char* str = (char*)shmat(shmid, (void*)0, 0);
 
-   if(argc == 1){  //Parent process
-      //Remove shared memory on construction and destruction
-      struct shm_remove 
-      {
-         shm_remove() { shared_memory_object::remove("MySharedMemory"); }
-         ~shm_remove(){ shared_memory_object::remove("MySharedMemory"); }
-      } remover;
+    std::cout<<str<<'\n';
 
-    // Create shared memory segment
-    boost::interprocess::managed_shared_memory segment(boost::interprocess::open_or_create, "MySharedMemory", 65536);
+    shmdt(str);
 
-    // Allocate space in the shared memory
-    char* shared_memory = segment.construct<char>("SharedMemory")[1024]();
+    shmctl(shmid, IPC_RMID, NULL);
+    
+}
+int main()
+{
+    // ftok to generate unique key
+    key_t key = ftok("shmfile", 65);
+ 
+    // shmget returns an identifier in shmid
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+ 
+    // shmat to attach to shared memory
+    char* str = (char*)shmat(shmid, (void*)0, 0);
+ 
+    std::cout << "Write Data : ";
+    std::cin.getline(str, 1024);
+ 
+    std::cout << "Data written in memory: " << str << std::endl;
+ 
+    // detach from shared memory
+    shmdt(str);
+ 
 
-    std::cout << "Server: Waiting for client data...\n";
-
-    // Wait for the client to write data
-    while (shared_memory[0] == 0) {
-        boost::interprocess::ipcdetail::thread_yield();
+    std::thread read_thread{read_shared_memory};
+    if(read_thread.joinable())
+    {
+        read_thread.join();
     }
-
-    std::cout << "Server: Received data from client: " << shared_memory << "\n";
-
-    // Modify the received data (e.g., convert to uppercase)
-    for (std::size_t i = 0; i < strlen(shared_memory); ++i) {
-        shared_memory[i] = toupper(shared_memory[i]);
-    }
-
-    std::cout << "Server: Modified data.\n";
-
     return 0;
-   }
-   else{
-      // Open the shared memory segment
-    boost::interprocess::managed_shared_memory segment(boost::interprocess::open_or_create, "MySharedMemory", 65536);
-
-    // Access the shared memory
-    char* shared_memory = segment.find<char>("SharedMemory").first;
-
-    // Write data to shared memory
-    const char* data = "Hello, server!";
-    strncpy(shared_memory, data, 1024);
-
-    std::cout << "Client: Sent data to server: " << shared_memory << "\n";
-
-    return 0;
-   }
-   return 0;
 }
