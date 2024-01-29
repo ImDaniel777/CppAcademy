f202484376954f5b8b5c059af81c1a30fb18b1d9
ImDaniel777 <iancudaniel99@yahoo.com>
2024-01-29 12:26:00 +0200

    move oop tasks to sepparate  branch

---
+++ oop/Car.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <iostream>
+
+class Car
+{
+private:
+    int mileage = 0;
+    int horsePower;
+    float kwPower;
+    std::string chassisType;
+protected:
+    std::string  *_VIN = nullptr;
+
+public:
+    Car();
+    Car(std::string  &VIN);
+
+    virtual void accelerate() = 0;
+    
+    int getMileage() const;
+    void setMileage(int value);
+
+    int getHorsePower() const;
+    void setHorsePower(int value);
+
+    float getKwPower()const;
+    void setKwPower(float value);
+
+    std::string getChassisType()const;
+    void setChassisType(std::string &value);
+
+    std::string  getVIN()const;
+    void setVIN(std::string value);
+
+    virtual ~Car();
+};
