f9455f8470a9f9defec8907269ec19fb7653a978
ImDaniel777 <iancudaniel99@yahoo.com>
2024-02-23 09:58:12 +0200

    generate code coverage

---
+++ Src/CMakeFiles/Cpp_Component_Test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cpp_Component_Test.dir/Cpp_Component_Test.cpp.o"
+  "bin/Cpp_Component_Test"
+  "bin/Cpp_Component_Test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cpp_Component_Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Src/CMakeFiles/nr_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nr_lib.dir/Cpp_Component.cpp.o"
+  "lib/libnr_lib.pdb"
+  "lib/libnr_lib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nr_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
